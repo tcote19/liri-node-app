3ec6970b21e94b628a766b34acca6d26d14299cb
thomas cote <tcote19@gmail.com>
2018-01-10 20:37:30 -0800

    added files

---
+++ scripts/key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '33tZzHBwwktNOCQc5Ndsg26zK',
+  consumer_secret: 'No7Aq0vJAiX2Gvzu2bH43dnqueOGfZN6zZcTftAXWHgEf44eCE',
+  access_token_key: '951309305717796865-UX5DyOO6aWgRslS3EirIghMR6YQHGQl',
+  access_token_secret: 'mzBlDui0j5NvmYC6sNgmxoEoW1lUL01CYnQWy3VBRxpkQ',
+}
+
+module.exports = twitterKeys;
