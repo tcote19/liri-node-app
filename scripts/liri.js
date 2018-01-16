e00d91dfdf7bf38267d5f9631bf464e55aee1d5e
thomas cote <tcote19@gmail.com>
2018-01-16 12:48:19 -0800

    Nearly completed scripts

--- scripts/liri.js
+++ scripts/liri.js
@@ -1,9 +1,5 @@
 var myTweet = process.argv[2];
 var spotifySong = process.argv[2];
-var spotifySongTitle1st = process.argv[3];
-var spotifySongTitle2nd = process.argv[4];
-var spotifySongTitle3rd = process.argv[5];
-var spotifySongTitle4th = process.argv[6];
 var spotifySongArtist = process.argv[4];
 var movieTime = process.argv[2];
 var doIt = process.argv[2];
@@ -14,15 +10,42 @@ var request = require("request");
 var twitterKeyItems = require("./key.js").client;
 var spotifyKeyItems = require("./key.js").patron;
 
-
 if (myTweet === "my-tweets"){
 	myTweetRuns();
 } else if (myTweet !== "my-tweets" && spotifySong !== "spotify-this-song"){
 	console.log("--------------------------");
 	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>");
-} else if (spotifySong === "spotify-this-song" && spotifySongTitle1st !== undefined ) {
-	spotifyStuff();
-} else if (spotifySong === "spotify-this-song" && spotifySongTitle1st === undefined && spotifySongTitle2nd === undefined && spotifySongTitle3rd === undefined && spotifySongTitle4th === undefined){
+} else if (spotifySong === "spotify-this-song" && process.argv[3] !== undefined && process.argv[4] === undefined && process.argv[5] === undefined && process.argv[6] === undefined) {
+		var spotifySongTitle1st = process.argv[3];
+		if (typeof spotifySongTitle1st === "string"){
+
+		spotifyStuffOneWord();
+		}
+} else if (spotifySong === "spotify-this-song" && process.argv[3] !== undefined && process.argv[4] !== undefined && process.argv[5] === undefined && process.argv[6] === undefined) {
+		var spotifySongTitle1st = process.argv[3];
+		var spotifySongTitle2nd = process.argv[4];
+		if (typeof spotifySongTitle1st === "string" && typeof spotifySongTitle2nd === "string"){
+
+		spotifyStuffTwoWord();
+		}
+} else if (spotifySong === "spotify-this-song" && process.argv[3] !== undefined && process.argv[4] !== undefined && process.argv[5] !== undefined && process.argv[6] === undefined ) {
+		var spotifySongTitle1st = process.argv[3];
+		var spotifySongTitle2nd = process.argv[4];
+		var spotifySongTitle3rd = process.argv[5];
+		if (typeof spotifySongTitle1st === "string" && typeof spotifySongTitle2nd === "string" && typeof spotifySongTitle3rd === "string"){
+
+		spotifyStuffThreeWord();
+		}
+} else if (spotifySong === "spotify-this-song" && process.argv[3] !== undefined && process.argv[4] !== undefined && process.argv[5] !== undefined && process.argv[6] !== undefined ) {
+		var spotifySongTitle1st = process.argv[3];
+		var spotifySongTitle2nd = process.argv[4];
+		var spotifySongTitle3rd = process.argv[5];
+		var spotifySongTitle4th = process.argv[6];
+		if (typeof spotifySongTitle1st === "string" && typeof spotifySongTitle2nd === "string" && typeof spotifySongTitle3rd === "string" && typeof spotifySongTitle4th === "string"){
+
+		spotifyStuffFourWord();
+		}		
+} else if (spotifySong === "spotify-this-song" && process.argv[3] === undefined && process.argv[4] === undefined && process.argv[5]=== undefined && process.argv[6] === undefined){
 	aceOfBase = "Ace "+"of "+"Base";
 	spotifyDeftFailure(); 
 } else {
@@ -50,7 +73,7 @@ function myTweetRuns(){
 
 // Spotify Functions
 
-function spotifyStuff(){
+function spotifyStuffOneWord(){
 	spotifyKeyItems.search({type: 'track', query: spotifySongTitle1st}, function(err, data) {
 	  if (err) {
 	    return console.log('Error occurred: ' + err);
@@ -63,6 +86,44 @@ function spotifyStuff(){
 	});
 };
 
+function spotifyStuffTwoWord(){
+	spotifyKeyItems.search({type: 'track', query: spotifySongTitle1st+" "+spotifySongTitle2nd}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	console.log("--------------------------");
+	console.log("Artist: "+data.tracks.items[0].artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	console.log("Link: "+data.tracks.items[0].preview_url);
+	});
+};
+
+function spotifyStuffThreeWord(){
+	spotifyKeyItems.search({type: 'track', query: spotifySongTitle1st+" "+spotifySongTitle2nd+" "+spotifySongTitle3rd}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	console.log("--------------------------");
+	console.log("Artist: "+data.tracks.items[0].artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	console.log("Link: "+data.tracks.items[0].preview_url);
+	});
+};
+
+function spotifyStuffFourWord(){
+	spotifyKeyItems.search({type: 'track', query: spotifySongTitle1st+" "+spotifySongTitle2nd+" "+spotifySongTitle3rd+" "+spotifySongTitle4th}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	console.log("--------------------------");
+	console.log("Artist: "+data.tracks.items[0].artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	console.log("Link: "+data.tracks.items[0].preview_url);
+	});
+};
 function spotifyDeftFailure(){
 	spotifyKeyItems.search({type: 'track', query: aceOfBase}, function(err, data) {
 	  if (err) {
@@ -77,5 +138,10 @@ function spotifyDeftFailure(){
 };
 
 
-// Still needing to add if/else statements to control user entering additional word/arguments during a song search. Currently one word
-// can be searched through spotify.
+
+
+
+
+
+
+
