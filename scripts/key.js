e2c171cdb65f8f339e705798e263f08deec2f10e
thomas cote <tcote19@gmail.com>
2018-01-15 16:02:46 -0800

    updated scripts

--- scripts/key.js
+++ scripts/key.js
@@ -1,5 +1,6 @@
 console.log('Data loaded');
 var Twitter = require('twitter');
+var Spotify = require("node-spotify-api");
 
 var client = new Twitter({
   consumer_key: '33tZzHBwwktNOCQc5Ndsg26zK',
@@ -7,19 +8,14 @@ var client = new Twitter({
   access_token_key: '951309305717796865-UX5DyOO6aWgRslS3EirIghMR6YQHGQl',
   access_token_secret: 'mzBlDui0j5NvmYC6sNgmxoEoW1lUL01CYnQWy3VBRxpkQ',
 });
-// var Spotify = require('node-spotify-api');
  
-// var spotify = new Spotify({
-//   id: 'ada95c8d0dc34557b598fe04c36e5322',
-//   secret: '9adb7b5e5d714cf287c298a1510e3809'
-// });
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
+var patron = new Spotify({
+  id: 'ada95c8d0dc34557b598fe04c36e5322',
+  secret: '9adb7b5e5d714cf287c298a1510e3809'
+});
 
-module.exports = client;
+
+module.exports = {
+	client: client,
+	patron: patron
+}
--- scripts/liri.js
+++ scripts/liri.js
@@ -1,27 +1,81 @@
 var myTweet = process.argv[2];
-var request = require("request");
-var twitterKeyItems = require("./key.js");
+var spotifySong = process.argv[2];
+var spotifySongTitle1st = process.argv[3];
+var spotifySongTitle2nd = process.argv[4];
+var spotifySongTitle3rd = process.argv[5];
+var spotifySongTitle4th = process.argv[6];
+var spotifySongArtist = process.argv[4];
+var movieTime = process.argv[2];
+var doIt = process.argv[2];
 
-if (myTweet === "my-tweet"){
+var aceOfBase = "";
+
+var request = require("request");
+var twitterKeyItems = require("./key.js").client;
+var spotifyKeyItems = require("./key.js").patron;
+
+
+if (myTweet === "my-tweets"){
 	myTweetRuns();
+} else if (myTweet !== "my-tweets" && spotifySong !== "spotify-this-song"){
+	console.log("--------------------------");
+	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>");
+} else if (spotifySong === "spotify-this-song" && spotifySongTitle1st !== undefined ) {
+	spotifyStuff();
+} else if (spotifySong === "spotify-this-song" && spotifySongTitle1st === undefined && spotifySongTitle2nd === undefined && spotifySongTitle3rd === undefined && spotifySongTitle4th === undefined){
+	aceOfBase = "Ace "+"of "+"Base";
+	spotifyDeftFailure(); 
 } else {
-	console.log("please enter my-tweet as your third argument.");
+	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>");
 }
 
+// Twitter Function
 
 function myTweetRuns(){
-var twitterName = {screen_name: 'thetrain1919'};
-twitterKeyItems.get('statuses/user_timeline', twitterName, function(error, tweets, response){
-	if (!error){
-		var tweetCount = tweets.length;
+	var twitterName = {screen_name: 'thetrain1919'};
+	twitterKeyItems.get('statuses/user_timeline', twitterName, function(error, tweets, response){
+		if (!error){
+			var tweetCount = tweets.length;
 
-		for (var i = 0; i < tweets.length; i++){
-			console.log("Tweet #" + tweetCount--);
-			console.log(tweets[i].text);
-			console.log(response.headers.date);
-			console.log("--------------------------");
+			for (var i = 0; i < tweets.length; i++){
+				console.log("Tweet #" + tweetCount--);
+				console.log(tweets[i].text);
+				console.log(response.headers.date);
+				console.log("--------------------------");
+			}
 		}
-	}
 
-});
-}
+	});
+};
+
+// Spotify Functions
+
+function spotifyStuff(){
+	spotifyKeyItems.search({type: 'track', query: spotifySongTitle1st}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	console.log("--------------------------");
+	console.log("Artist: "+data.tracks.items[0].artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	console.log("Link: "+data.tracks.items[0].preview_url);
+	});
+};
+
+function spotifyDeftFailure(){
+	spotifyKeyItems.search({type: 'track', query: aceOfBase}, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+	console.log("--------------------------");
+	console.log("Artist: "+data.tracks.items[0].artists[0].name);
+	console.log("Song: "+data.tracks.items[0].name);
+	console.log("Album: "+data.tracks.items[0].album.name);
+	console.log("Link: "+data.tracks.items[0].preview_url);
+	});
+};
+
+
+// Still needing to add if/else statements to control user entering additional word/arguments during a song search. Currently one word
+// can be searched through spotify.
