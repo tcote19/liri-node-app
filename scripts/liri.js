339afc6e5adcd8705965b609d058ee297a68f64b
thomas cote <tcote19@gmail.com>
2018-01-17 13:03:42 -0800

    Updated

--- scripts/liri.js
+++ scripts/liri.js
@@ -1,7 +1,8 @@
 var myTweet = process.argv[2];
 var spotifySong = process.argv[2];
-var spotifySongArtist = process.argv[4];
-var movieTime = process.argv[2];
+// var spotifySongArtist = process.argv[4];
+var moveieInit = process.argv[2];
+var movieName = process.argv.splice(3).join('+');
 var doIt = process.argv[2];
 
 var aceOfBase = "";
@@ -12,9 +13,9 @@ var spotifyKeyItems = require("./key.js").patron;
 
 if (myTweet === "my-tweets"){
 	myTweetRuns();
-} else if (myTweet !== "my-tweets" && spotifySong !== "spotify-this-song"){
+} else if (myTweet !== "my-tweets" && spotifySong !== "spotify-this-song" && moveieInit !== "movie-this"){
 	console.log("--------------------------");
-	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>");
+	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>\nmovie-this");
 } else if (spotifySong === "spotify-this-song" && process.argv[3] !== undefined && process.argv[4] === undefined && process.argv[5] === undefined && process.argv[6] === undefined) {
 		var spotifySongTitle1st = process.argv[3];
 		if (typeof spotifySongTitle1st === "string"){
@@ -48,6 +49,8 @@ if (myTweet === "my-tweets"){
 } else if (spotifySong === "spotify-this-song" && process.argv[3] === undefined && process.argv[4] === undefined && process.argv[5]=== undefined && process.argv[6] === undefined){
 	aceOfBase = "Ace "+"of "+"Base";
 	spotifyDeftFailure(); 
+} else if (moveieInit === "movie-this" && movieName !== undefined){
+	omdbTime();
 } else {
 	console.log("ERROR: Please enter one of the following commands\nmy-tweets\nspotify-this-song <song title>");
 }
@@ -137,8 +140,38 @@ function spotifyDeftFailure(){
 	});
 };
 
+// OMDB Movie Functions
 
+function omdbTime(){
+	console.log(movieName);
+request("http://www.omdbapi.com/?t="+movieName+"&y=&plot=short&apikey=trilogy", function(error, response, body) {
 
+  if (!error && response.statusCode === 200) {
+  	console.log("--------------------------");
+  	console.log("Movie Title: "+JSON.parse(body).Title);
+  	console.log("Year: "+JSON.parse(body).Year);
+    
+    if (JSON.parse(body).Ratings[0,1,2,3,4,5] !== true){
+    	console.log("IMDB Rating: "+JSON.parse(body).imdbRating);
+    	console.log("Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1].Value);
+    } else {
+    	console.log("No ratings listed");
+	};
+    console.log("Produced in: "+JSON.parse(body).Country);
+    console.log("Language: "+JSON.parse(body).Language);
+    console.log("Plot: "+JSON.parse(body).Plot);
+    console.log("Actors: "+JSON.parse(body).Actors);
+  }
+});
+}
+   // * Title of the movie.
+   // * Year the movie came out.
+   // * IMDB Rating of the movie.
+   // * Rotten Tomatoes Rating of the movie.
+   // * Country where the movie was produced.
+   // * Language of the movie.
+   // * Plot of the movie.
+   // * Actors in the movie.
 
 
 
